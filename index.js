ee43c77c773ff9331ee48ca9b2ada58f1bc8e515
Ali Azimi <112652386+aliazim1@users.noreply.github.com>
2023-04-03 23:17:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-let navIcon = document.querySelector('.menu-icon');
-let navMenu = document.querySelector('.nav-menu');
-let exitIcon = document.querySelector('.exit-icon');
+// let navIcon = document.querySelector('.menu-icon');
+// let navMenu = document.querySelector('.nav-menu');
+// let exitIcon = document.querySelector('.exit-icon');
 
-navIcon.addEventListener("click", () => {
-    navIcon.classList.toggle('active');
-    navMenu.classList.toggle('active');
-    exitIcon.classList.toggle('active');
-})
+// navIcon.addEventListener("click", () => {
+//     navIcon.classList.toggle('active');
+//     navMenu.classList.toggle('active');
+//     exitIcon.classList.toggle('active');
+// })
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",
-() => {
-    navIcon.classList.remove('active');
-    navMenu.classList.remove('active');
-}))
-
-
+// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",
+// () => {
+//     navIcon.classList.remove('active');
+//     navMenu.classList.remove('active');
+// }))
 
 window.addEventListener("load", function() {
     const loader = this.document.querySelector(".loader");
