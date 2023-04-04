997a2c4ecb003d70f77056020f1e6f4ea75848d4
Ali Azimi <112652386+aliazim1@users.noreply.github.com>
2023-04-04 15:59:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
- let navIcon = document.querySelector('.menu-icon');
- let navMenu = document.querySelector('.nav-menu');
- let exitIcon = document.querySelector('.exit-icon');
+let navIcon = document.querySelector('.menu-icon');
+let navMenu = document.querySelector('.nav-menu');
+let exitIcon = document.querySelector('.exit-icon');
 
- navIcon.addEventListener("click", () => {
-     navIcon.classList.toggle('active');
-     navMenu.classList.toggle('active');
-     exitIcon.classList.toggle('active');
- })
+navIcon.addEventListener("click", () => {
+    navIcon.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    exitIcon.classList.toggle('active');
+})
 
- document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",
- () => {
-     navIcon.classList.remove('active');
-     navMenu.classList.remove('active');
- }))
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",
+() => {
+    navIcon.classList.remove('active');
+    navMenu.classList.remove('active');
+}))
