01956450d58f0aa24b4aea6898bd15cbc71ff5b4
Ali Azimi <112652386+aliazim1@users.noreply.github.com>
2023-04-04 00:43:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-// let navIcon = document.querySelector('.menu-icon');
-// let navMenu = document.querySelector('.nav-menu');
-// let exitIcon = document.querySelector('.exit-icon');
+ let navIcon = document.querySelector('.menu-icon');
+ let navMenu = document.querySelector('.nav-menu');
+ let exitIcon = document.querySelector('.exit-icon');
 
-// navIcon.addEventListener("click", () => {
-//     navIcon.classList.toggle('active');
-//     navMenu.classList.toggle('active');
-//     exitIcon.classList.toggle('active');
-// })
+ navIcon.addEventListener("click", () => {
+     navIcon.classList.toggle('active');
+     navMenu.classList.toggle('active');
+     exitIcon.classList.toggle('active');
+ })
 
-// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",
-// () => {
-//     navIcon.classList.remove('active');
-//     navMenu.classList.remove('active');
-// }))
-
-window.addEventListener("load", function() {
-    const navbar = this.document.querySelector("nav");
-    const loader = this.document.querySelector(".loader");
-    navbar.classList.add("navbar");
-    loader.classList.add("loader-hidden");
-
-    loader.addEventListener("transitionend", function () {
-        document.body.removeChild("loader");
-    })
-})
+ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",
+ () => {
+     navIcon.classList.remove('active');
+     navMenu.classList.remove('active');
+ }))
